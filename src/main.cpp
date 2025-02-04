5da96652a541f917ebd86ee53c6df9be58ecffba
Ali Raza Khalid <razamindset.official@gmail.com>
2025-02-04 19:31:02 +0500

    Chore: Simple game play using command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,67 @@
-
 #include "chess.hpp"
 
 using namespace chess;
 
 int main() {
-  Board board =
-      Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
+  Board board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   Movelist moves;
-  movegen::legalmoves(moves, board);
+  std::string imove;
 
-  for (const auto &move : moves) {
-    std::cout << uci::moveToUci(move) << std::endl;
+  std::cout << "Initial Board:\n" << board << "\n";
+
+  while (true) {
+    movegen::legalmoves(moves, board);  // Generate legal moves
+    for (auto &&legal : moves) {
+      std::cout << uci::moveToUci(legal) << "\t";
+    }
+
+    std::cout << "\n";
+
+    std::cout << "Enter move in UCI format (e.g., e2e4): ";
+    std::cin >> imove;
+
+    bool moveMade = false;
+    for (auto &&move : moves) {
+      if (uci::moveToUci(move) == imove) {
+        board.makeMove(move);
+        moveMade = true;
+        break;  // Exit the loop once a valid move is made
+      }
+    }
+
+    if (!moveMade) {
+      std::cout << "Invalid move, try again.\n";
+      continue;
+    }
+
+    std::pair<chess::GameResultReason, chess::GameResult> result =
+        board.isGameOver();
+
+    if (result.second != GameResult::NONE) {
+      std::cout << "Game Over! ";
+
+      if (result.first == GameResultReason::CHECKMATE) {
+        std::cout << "Checkmate! ";
+
+        if (board.sideToMove() == Color::WHITE) {
+          std::cout << "Black Wins.\n";
+        } else {
+          std::cout << "White Wins.\n";
+        }
+
+      } else if (result.first == GameResultReason::STALEMATE) {
+        std::cout << "Stalemate! It's a Draw.\n";
+      } else if (result.first == GameResultReason::INSUFFICIENT_MATERIAL) {
+        std::cout << "Insufficient Material! It's a Draw.\n";
+      } else if (result.first == GameResultReason::FIFTY_MOVE_RULE) {
+        std::cout << "Fifty-Move Rule! It's a Draw.\n";
+      } else if (result.first == GameResultReason::THREEFOLD_REPETITION) {
+        std::cout << "Threefold Repetition! It's a Draw.\n";
+      }
+      break;
+    }
+
+    std::cout << "After Board:\n" << board << "\n";
   }
 
   return 0;
